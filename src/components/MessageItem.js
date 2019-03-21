ddd61f67adae7ddf0898230635d5637d5fb67952
nooknpj <30778382+nooknpj@users.noreply.github.com>
2019-03-21 15:30:43 +0700

    change message item style to have color
    change message item style to have color

--- src/components/MessageItem.js
+++ src/components/MessageItem.js
@@ -1,10 +1,40 @@
 import React, { Component } from "react";
 
 export class MessageItem extends Component {
+  getMessageStlye = () => {
+    let messageStyle = {
+      marginLeft: "-10px",
+      marginRight: "-10px",
+      paddingTop: "10px",
+      paddingBottom: "10px",
+      paddingLeft: "20px",
+      color: "white"
+    };
+    let userID = localStorage.getItem("userID");
+    if (userID == this.props.userID) {
+      messageStyle["backgroundColor"] = "#31B700";
+    } else {
+      messageStyle["backgroundColor"] = "#0085CA";
+    }
+
+    return messageStyle;
+  };
+
+  getMessageHeaderStlye = () => {
+    let messageStyle = {};
+    let userID = localStorage.getItem("userID");
+    if (userID == this.props.userID) {
+      messageStyle["backgroundColor"] = "#ADDC91";
+    } else {
+      messageStyle["backgroundColor"] = "#8BD3E6";
+    }
+
+    return messageStyle;
+  };
   render() {
     return (
-      <div className="messageDiv">
-        <div style={messageHeaderStyle}>
+      <div className="messageDiv" style={this.getMessageHeaderStlye()}>
+        <div style={this.getMessageHeaderStlye()}>
           <span style={{ marginLeft: "10px" }}> ClientID: </span>
           <span> {this.props.userID}</span>
           <div style={{ float: "right", marginRight: "10px" }}>
@@ -13,7 +43,9 @@ export class MessageItem extends Component {
           </div>
         </div>
 
-        <span>{this.props.content}</span>
+        <div style={this.getMessageStlye()}>
+          <span>{this.props.content}</span>
+        </div>
       </div>
     );
   }
